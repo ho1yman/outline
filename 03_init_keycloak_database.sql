b81ae05c1214aa2e52398527f8e269cebfa37272
ho1yman <93108144+ho1yman@users.noreply.github.com>
2025-04-22 05:52:49 +0800

    更新 03_init_keycloak_database.sql

--- 03_init_keycloak_database.sql
+++ 03_init_keycloak_database.sql
@@ -1,8 +1,8 @@
--- Active: 1745161958981@@192.168.63.147@54322@outline
-# create role
+
+--# create role
 CREATE ROLE "keycloak" SUPERUSER CREATEDB CREATEROLE LOGIN REPLICATION BYPASSRLS PASSWORD 'your_password';
 
-# create db
+--# create db
 CREATE DATABASE "keycloak"
 WITH
   OWNER = "keycloak"
