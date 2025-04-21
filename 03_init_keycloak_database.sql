06faf66b655fe57e4ba8d8ad1e6d51bacd9454a2
ho1yman <93108144+ho1yman@users.noreply.github.com>
2025-04-21 22:43:09 +0800

    Add files via upload

---
+++ 03_init_keycloak_database.sql
@@ -0,0 +1,10 @@
+-- Active: 1745161958981@@192.168.63.147@54322@outline
+# create role
+CREATE ROLE "keycloak" SUPERUSER CREATEDB CREATEROLE LOGIN REPLICATION BYPASSRLS PASSWORD 'passw0rd';
+
+# create db
+CREATE DATABASE "keycloak"
+WITH
+  OWNER = "keycloak"
+  ENCODING = 'UTF8'
+  TABLESPACE = "pg_default";
